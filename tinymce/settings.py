310454718ad37349933327d84bdf13e99eeddb20
nikitatonkoshkur <tonkoshkur@oneplanetops.com>
2021-03-10 14:17:15 +0200

    settings fix

--- tinymce/settings.py
+++ tinymce/settings.py
@@ -1,33 +1,69 @@
-import os
-import warnings
+# License: MIT, see LICENSE.txt
+"""The django-tinymce4-lite configuration options"""
+
+import re
+import sys
 from django.conf import settings
 from django.contrib.staticfiles.storage import staticfiles_storage
-from django.utils.functional import lazy
-from tinymce.utils import baseurl
 
-OLD_CONFIG = getattr(settings, 'TINYMCE_DEFAULT_CONFIG', False)
-if OLD_CONFIG:
-    warnings.warn("TINYMCE_DEFAULT_CONFIG is deprecated, check docs for instructions.", DeprecationWarning)
-DEFAULT_CONFIG = OLD_CONFIG or {'theme': "modern", 'relative_urls': False}
 
-if getattr(settings, 'TINYMCE_JS_URL', False):
-    raise RuntimeError("TINYMCE_JS_URL is not supported anymore, check docs for instructions.")
+def is_managed():
+    """
+    Check if a Django project is being managed with ``manage.py`` or
+    ``django-admin`` scripts
 
-# added laziness to support django.contrib.staticfiles.storage.ManifestStaticFilesStorage
-# and other storages that needs post-processing
-staticfiles_storage_url_lazy = lazy(staticfiles_storage.url, str)
-JS_URL = staticfiles_storage_url_lazy('tinymce/tinymce.min.js')
-JS_ROOT = staticfiles_storage_url_lazy('tinymce')
-JS_BASE_URL = lazy(baseurl, str)(JS_URL)
+    :return: Check result
+    :rtype: bool
+    """
+    for item in sys.argv:
+        if re.search(r'manage.py|django-admin|django', item) is not None:
+            return True
+    return False
 
-if getattr(settings, 'TINYMCE_SPELLCHECKER', False):
-    raise NotImplementedError("TINYMCE_SPELLCHECKER is not implemented yet.")
-USE_SPELLCHECKER = False
 
-if getattr(settings, 'TINYMCE_COMPRESSOR', False):
-    raise NotImplementedError("TINYMCE_COMPRESSOR is not implemented yet.")
-USE_COMPRESSOR = False
+DEFAULT = {
+    'selector': 'textarea',
+    'theme': 'modern',
+    'plugins': 'link image preview codesample contextmenu table code lists',
+    'toolbar1': 'formatselect | bold italic underline | alignleft aligncenter alignright alignjustify '
+               '| bullist numlist | outdent indent | table | link image | codesample | preview code',
+    'contextmenu': 'formats | link image',
+    'menubar': False,
+    'inline': False,
+    'statusbar': True,
+    'width': 'auto',
+    'height': 360,
+}
+"""Default TinyMCE 4 configuration"""
+USE_SPELLCHECKER = getattr(settings, 'TINYMCE_SPELLCHECKER', False)
+"""Use tinymce4 built-in spellchecker service"""
+if USE_SPELLCHECKER:
+    DEFAULT['plugins'] += ' spellchecker'
+    DEFAULT['toolbar1'] += ' | spellchecker'
+CONFIG = getattr(settings, 'TINYMCE_DEFAULT_CONFIG', DEFAULT)
+"""TinyMCE 4 configuration"""
+JS_URL = getattr(settings, 'TINYMCE_JS_URL', None)
+"""TinyMCE 4 JavaScript code"""
+if JS_URL is None:
+    # Ugly hack that allows to run management commands with ManifestStaticFilesStorage
+    _orig_debug = settings.DEBUG
+    if is_managed():
+        settings.DEBUG = True
+    JS_URL = staticfiles_storage.url('tinymce/js/tinymce/tinymce.min.js')
+    settings.DEBUG = _orig_debug
+ADDIONAL_JS_URLS = getattr(settings, 'TINYMCE_ADDITIONAL_JS_URLS', None)
+"""Additional JS files for TinyMCE (e.g. custom plugins)"""
+CSS_URL = getattr(settings, 'TINYMCE_CSS_URL', None)
+"""
+Additional CSS styles for TinyMCE 4
 
-if getattr(settings, 'TINYMCE_FILEBROWSER', False):
-    raise RuntimeError("TINYMCE_FILEBROWSER is not supported anymore, check docs for instructions.")
-USE_FILEBROWSER = False
+The default CSS is used to fix TinyMCE 4 position in Django Admin.
+"""
+CALLBACKS = getattr(settings, 'TINYMCE_CALLBACKS', {})
+"""TinyMCE 4 calback JavaScript functions"""
+USE_FILEBROWSER = getattr(settings, 'TINYMCE_FILEBROWSER', 'filebrowser' in settings.INSTALLED_APPS)
+"""
+Enable integration with `django-filebrowser-no-grappelli`_.
+
+.. _django-filebrowser-no-grappelli: https://github.com/smacker/django-filebrowser-no-grappelli
+"""
